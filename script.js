1a2a66f7344843af1f369f548b071fda95702680
Farwa Tariq <farwa.tariq2434@gmail.com>
2025-05-18 21:48:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+
+alert("Hello there! 😊 We're thrilled to have you here. Enjoy exploring our website!");
+
+// 2. Write a script to display following message on your web
+// page:
+alert("Error! Please Enter a valid Password");
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+alert("Welcone To JS Land... \n Happy Coding!😊");
+let response = confirm("☠️ Do you want to continue your coding journey?");
+
+        if (response) {
+            alert("💻 Brave soul! Happy Coding... while it lasts...");
+        } else {
+            alert("😨 Coward! Run before the bugs catch you!");
+        }
+
+// 4. Write a script to display following messages in sequence:
+alert("Welcome To JS Land....");
+alert("Happy Coding!!! \n " , confirm("Are You Sure!😈"));
+
+// 5. Generate the following message through browser’s
+// developer console:
+alert("HELLO! I can run JS through my web browser console");
+console.log("HELLO! I can run JS through my web browser console");
